a9a62e83f920937d30472bdc0b9d63afe11e5edb
AlexAndorra <AlexAndorra@users.noreply.github.com>
2021-01-13 17:26:43 +0000

    deploy: 8a31c05d4a4180429084601f7bf8380ae7e487b9

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/pollsposition_website/assets/js/search-data.json', true);
+    request.open('GET', '/pollsposition_blog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
